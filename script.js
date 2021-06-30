8e235d28cecb5af8ba7cc09a7a9a45fad879f50e
Glitch (wax-walnut-joke) <none>
2021-06-30 15:36:09 +0000

    😼🔔 Updated with Glitch

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const initializationRequest = dataArr => {
     source: "Kustomer"
   };
 
-  fetch("http://104.154.194.160:80/csatai/initialization", {
+  fetch("https://api.csat.ai/csatai/initialization", {
     method: "POST",
     mode: "cors",
     headers: {
@@ -23,6 +23,8 @@ const initializationRequest = dataArr => {
 
 const initializationResponse = () => {};
 
+const evaluationResponse = res => {};
+
 Kustomer.initialize(conversation => {
   console.log(conversation);
   var attributes = conversation.attributes;
@@ -33,23 +35,53 @@ Kustomer.initialize(conversation => {
   var userUrl = conversation.relationships.modifiedBy.links.self;
 
   let contextData = getContext(customerUrl, messagesUrl, orgUrl, userUrl);
-  initializationRequest(contextData);
+  // Kustomer.on("kustomer.message.create", function(context) {
+  //   console.log(context);
+  // });
+  // Kustomer.on("kustomer.message.update", function(context) {
+  //   console.log(context);
+  // });
+  // Kustomer.on("kustomer.conversation.update", function(context) {
+  //   console.log(context);
+  // });
+  Kustomer.on("context", function(context) {
+    let eval_obj = {
+      current_answer: context.attributes.preview,
+      status: context.attributes.status,
+      timestamp: context.attributes.updatedAt,
+      direction: context.attributes.direction,
+      endedAt:
+        context.attributes.status == "done" ? context.attributes.endedAt : "",
+      endedByType:
+        context.attributes.status == "done"
+          ? context.attributes.endedByType
+          : "",
+      addInfo: context.attributes.lastDone
+    };
+    fetch("https://api.csat.ai/csatai/evaluation/v1", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify(eval_obj),
+      keepalive: true
+    })
+      .then(res => evaluationResponse(res))
+      .catch(err => console.trace(err));
+    [...document.querySelector("#star-wow_experience").children].forEach(star =>
+      star.classList.replace("fal", "fas")
+    );
+    document.querySelector("#star-wow_experience").style.color = "#4f2c78";
 
-  // let customers = await CustomerDetails();
-  // let messages =  await MessageDetails();
-  // let org = await OrgDetails();
-  // let user = await UserDetails();
+    console.log(context);
+  });
+});
 
-  //   Promise.all(
-  //     [CustomerDetails, MessageDetails, OrgDetails, UserDetails]).then(details => {
-  //       console.log(details);
-  //       let dataObj = {};
-  //
+async function getContext(customerUrl, messagesUrl, orgUrl, userUrl) {
+  let customerData, messagesData, orgData, userData;
 
-  // }).catch((err)=>console.trace(err))
-
-  function getContext(customerUrl, messagesUrl, orgUrl, userUrl) {
-    let customerData, messagesData, orgData, userData;
+  let CustomerDetails = () =>
     Kustomer.request(
       {
         url: customerUrl,
@@ -65,6 +97,7 @@ Kustomer.initialize(conversation => {
       }
     );
 
+  let MessageDetails = () =>
     Kustomer.request(
       {
         url: messagesUrl,
@@ -80,6 +113,7 @@ Kustomer.initialize(conversation => {
       }
     );
 
+  let OrgDetails = () =>
     Kustomer.request(
       {
         url: orgUrl,
@@ -95,6 +129,7 @@ Kustomer.initialize(conversation => {
       }
     );
 
+  let UserDetails = () =>
     Kustomer.request(
       {
         url: userUrl,
@@ -110,6 +145,17 @@ Kustomer.initialize(conversation => {
       }
     );
 
-    return [customerData, messagesData, orgData, userData];
-  }
-});
+  let customers = await CustomerDetails();
+  let messages = await MessageDetails();
+  let org = await OrgDetails();
+  let user = await UserDetails();
+  await initializationRequest([customerData, messagesData, orgData, userData]);
+
+  // Promise.all([CustomerDetails, MessageDetails, OrgDetails, UserDetails])
+  //   .then(details => {
+  //     console.log(details);
+  //     return details;
+  //     // return [customerData, messagesData, orgData, userData];
+  //   })
+  //   .catch(err => console.trace(err));
+}
